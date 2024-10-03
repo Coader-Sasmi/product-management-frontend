8e25dd8855c0cd478bb5a4e39fccfe9d68cf83ff
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2024-10-03 19:43:39 +0530

    no fond page design done

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-screen w-full flex flex-col gap-5 justify-center items-center">
+      <img src="./not_found.png" alt="not found image" className="w-auto" />
+      <Link href="/">
+        <button className="btn bg-primary rounded-md uppercase">
+          back to home
+        </button>
+      </Link>
+    </div>
+  );
 }
